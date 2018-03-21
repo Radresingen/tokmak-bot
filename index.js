c1e7dffa1e067a540be1ba7cfa1f11078836927f
Şahin Olut <olut.sahin@gmail.com>
2018-03-21 23:31:57 +0000

    Reduced restriction

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ bot.on('sticker', async ctx => {
   try{
     if( ctx.session[ctx.chat.id] ){
       await ctx.telegram.restrictChatMember(ctx.chat.id, ctx.from.id, {
-        until_date : Date.now() + 3600000, // 1h
+        until_date : Date.now() + 600000, // 10m
         can_send_messages: true,
         can_send_media_messages: false,
         can_send_other_messages: false,
         can_send_web_page_previews: false
       });
-      return ctx.reply(`@${ctx.message.from.username} TOKMAKLANDIN`);
+      return ctx.message.from.username !== 'norveclibalikci' ? ctx.reply(`@${ctx.message.from.username} TOKMAKLANDIN`) : {}
     }
     ctx.session[ctx.chat.id] = true;
   } catch (err) {
