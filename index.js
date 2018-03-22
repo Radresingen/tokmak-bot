5f1fe7bef1a8ce054488540d0d5f76a869070fbb
Burak Bilge Yalçınkaya <bbyalcinkaya@gmail.com>
2018-03-23 00:18:04 +0300

    disable delete message

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ bot.on('sticker', async ctx => {
         can_send_web_page_previews: false
       });
       //await ctx.deleteMessage();
-      return ctx.reply(`@${ctx.message.from.username} TOKMAKLANDIN`) : {}
+      return ctx.reply(`@${ctx.message.from.username} TOKMAKLANDIN`);
     }
     ctx.session[ctx.chat.id] = true;
   } catch (err) {
